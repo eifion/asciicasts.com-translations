b6d1fe5d0a6651e06a011ed4adfd3fcf77a41e41
defaude <git@defaude.info>
2010-04-20 00:26:56 +0200

    renamed episode directories

--- renamer.rb
+++ renamer.rb
@@ -1,3 +1,5 @@
-Dir.foreach('./episodes') { |entry|
-  puts entry
-}
+Dir.chdir('./episodes')
+entries = Dir.entries('.').each do |old|
+  next if !(/^\d{1,3}$/.match(old))
+  File.rename(old, "%03d" % old)
+end
